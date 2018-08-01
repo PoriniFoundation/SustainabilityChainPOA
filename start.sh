3c503f2dc392b36b9c28703b36633ab6ffae6a2b
Porini Foundation <33342940+PoriniFoundation@users.noreply.github.com>
2018-08-01 21:04:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
 
+geth --datadir node1/ init files/genesis001.json &&
+geth --datadir node2/ init files/genesis001.json &&
+geth --datadir node3/ init files/genesis001.json &&
+geth --datadir node1/ --syncmode 'full' --port 30415 --rpc --rpcaddr 'localhost' --rpcport 8555 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes 'enode://cffd45e1d3b61f82607399672be2f02e947327ff828522ca9a8f3b7f72981f74e31d75f75d858480cb76d048f7ce8a59e7e28edeaf38e66b35ec1d5b197d719b@[194.230.110.57]:30413' --networkid 19760307001  &&
+
