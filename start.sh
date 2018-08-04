5e8ffe053738ba6e266715d5499ef4db0ecb1f89
Porini Foundation <33342940+PoriniFoundation@users.noreply.github.com>
2018-08-04 15:29:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ git clone https://github.com/PoriniFoundation/SustainabilityChainPOA/ &&
 cd SustainabilityChainPOA &&
 mkdir node1 &&
 geth --datadir node1/ init files/genesis001.json &&
-geth --datadir ./ --syncmode 'full' --port 30411 --rpc --rpcaddr 'localhost' --rpcport 8551 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes 'enode://45edb69e3be951065f3425f9fe35591d52e906359e10dbaf1b7a2259c9772022b79831f2f9836d54d607ef837d319fc4f52cce991489ba9babd3602f2289e4eb@[52.59.201.140]:30411' --networkid 19760307001 & &&
+nohup geth --datadir ./ --syncmode 'full' --port 30411 --rpc --rpcaddr 'localhost' --rpcport 8551 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes 'enode://45edb69e3be951065f3425f9fe35591d52e906359e10dbaf1b7a2259c9772022b79831f2f9836d54d607ef837d319fc4f52cce991489ba9babd3602f2289e4eb@[52.59.201.140]:30411' --networkid 19760307001 & &&
