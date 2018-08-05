f4f69551931e768c8d3895e39be7ee7c5e308a75
Porini Foundation <33342940+PoriniFoundation@users.noreply.github.com>
2018-08-05 11:16:04 +0200

    Update start.js

--- files/scripts/start.js
+++ files/scripts/start.js
@@ -1,6 +1,6 @@
 function start() {
 
-  #here you can add everything you want to do at the start
+ // #here you can add everything you want to do at the start
   
 loadScript("../files/scripts/loadBootnodes.js");
 loadBootnodes;
